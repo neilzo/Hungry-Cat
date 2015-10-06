44dd146dbd98c03624b33f305dff3e9e14f5af40
Neil Daftary <nmd278@nyu.edu>
2015-10-06 15:22:51 -0400

    Add get current user location.

--- client/app/app.js
+++ client/app/app.js
@@ -3,6 +3,27 @@
 
   function initialize(){
     console.log('working!');
+    getUserLocation();
+  }
+
+  function getUserLocation() {
+    var startPos;
+
+    var geoSuccess = function(position) {
+        startPos = position;
+        document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+        document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+    };
+
+    var geoError = function(error) {
+        alert('Error getting yo location. Error code: ' + error.code);
+        // error.code can be:
+        //   0: unknown error
+        //   1: permission denied
+        //   2: position unavailable (error response from location provider)
+        //   3: timed out
+      };
+    return navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
   }
 
   initialize();
