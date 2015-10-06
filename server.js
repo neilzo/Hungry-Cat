d0f23c031f841f06a348a2df14a88421819aeea2
Neil Daftary <nmd278@nyu.edu>
2015-10-06 14:36:03 -0400

    Add secrets for yelp.

--- server.js
+++ server.js
@@ -4,11 +4,10 @@
   var express = require('express');
   var reloader = require('connect-livereload');
   var app = express();
+  var yelp = require('./secrets.js');
 
   app.use(reloader());
   app.use(express.static('./client'));
-
-
 
   app.listen(9000, function(){
     console.log('App Listening on localhost:9000');
