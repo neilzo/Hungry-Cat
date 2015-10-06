d8fb9bd5a5fc07d18db66c5693aa07280a0f4134
Neil Daftary <nmd278@nyu.edu>
2015-10-06 15:16:22 -0400

    Add api url.

--- server.js
+++ server.js
@@ -4,8 +4,27 @@
   var express = require('express');
   var reloader = require('connect-livereload');
   var app = express();
-  var yelp = require('./secrets.js');
+  var yelpSecrets = require('./secrets.js');
 
+  var router = express.Router();
+
+  router.get('/', function(req, res){
+    res.json({ message: 'hooray this works'});
+  });
+
+  var yelp = require('yelp').createClient({
+    consumer_key: yelpSecrets.consumer_key,
+    consumer_secret: yelpSecrets.consumer_secret,
+    token: yelpSecrets.token,
+    token_secret: yelpSecrets.token_secret
+  });
+
+  // yelp.search({term: "food", location: "New York"}, function(error, data) {
+  //   console.log(error);
+  //   console.log(data);
+  // });
+  
+  app.use('/api', router);
   app.use(reloader());
   app.use(express.static('./client'));
 
