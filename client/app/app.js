591dbf653fb9549136a51ed638d27a8ebf64673a
Neil Daftary <nmd278@nyu.edu>
2015-10-06 15:32:08 -0400

    Scaffold basic vanilla ajax request.

--- client/app/app.js
+++ client/app/app.js
@@ -4,6 +4,7 @@
   function initialize(){
     console.log('working!');
     getUserLocation();
+    findFood();
   }
 
   function getUserLocation() {
@@ -24,6 +25,29 @@
         //   3: timed out
       };
     return navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+  }
+
+  function findFood(userLat, userLon) {
+    var request = new XMLHttpRequest();
+    request.open('GET', '/api', true);
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        var data = JSON.parse(request.responseText);
+        console.log(data);
+      } else {
+        // We reached our target server, but it returned an error
+        alert(request.responseText);
+      }
+    };
+
+    request.onerror = function() {
+      // There was a connection error of some sort
+      alert('Error sending your request');
+    };
+
+    request.send();
   }
 
   initialize();
