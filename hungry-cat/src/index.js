1fea38a93671d004b88113ee6fec2088390e26d0
Neil Daftary <nmd278@nyu.edu>
2017-01-02 13:07:04 -0500

    Fake loading.

--- hungry-cat/src/index.js
+++ hungry-cat/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Promise from 'promise'; // eslint-disable-line no-unused-vars
 import App from './App';
 import './index.css';
 
