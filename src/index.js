89effa49daed26dc9e3204cd264ff4d5efee448f
Neil Daftary <nmd278@nyu.edu>
2017-01-06 20:52:12 -0500

    Remove redux I don't need it and I already know it.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'promise';
-import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
-import initializeStore from './store';
-
-const initialState = {
-    results: [],
-    lat: '',
-    long: '',
-};
-
-const store = initializeStore(initialState);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-  document.getElementById('root')
+    <App />
+    document.getElementById('root')
 );
