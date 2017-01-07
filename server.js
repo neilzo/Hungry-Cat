48660851c2e3331fbf9ee204465c97026df21b5a
Neil Daftary <nmd278@nyu.edu>
2017-01-07 16:32:01 -0500

    ES6 :raised_hands:

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 (function() {
     'use strict';
-    var express = require('express');
-    var app = express();
-    var config = require('./secrets');
-    var bodyParser = require('body-parser');
-    var Yelp = require('yelp');
+    const express = require('express');
+    const app = express();
+    const config = require('./secrets');
+    const bodyParser = require('body-parser');
+    const Yelp = require('yelp');
 
-    var router = express.Router();
+    const router = express.Router();
 
     app.use(bodyParser.json()); // for parsing application/json
 
-    var yelp = new Yelp({consumer_key: config.yelp.consumer_key, consumer_secret: config.yelp.consumer_secret, token: config.yelp.token, token_secret: config.yelp.token_secret});
+    const yelp = new Yelp({consumer_key: config.yelp.consumer_key, consumer_secret: config.yelp.consumer_secret, token: config.yelp.token, token_secret: config.yelp.token_secret});
 
-    router.use(function(req, res, next) {
+    router.use((req, res, next) => {
         console.log('happenings!');
         next();
     });
 
-    router.route('/test').get(function(req, res, next) {
+    router.route('/test').get((req, res, next) => {
         res.send({message: 'OK!'});
     });
 
     router.route('/food').post(function(req, res) {
         console.log(req.body);
-        var ll = req.body.lat + ',' + req.body.long;
-        // var offset = req.query.offset;
-        var radius = 1609.34; //1 mile in meters
+        const ll = req.body.lat + ',' + req.body.long;
+        // const offset = req.query.offset;
+        const radius = 1609.34; //1 mile in meters
 
         //sort: 1 sorts by distance
         yelp.search({term: 'food', sort: 1, ll: ll, radius: radius}).then((data) => {
