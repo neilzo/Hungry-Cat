ba96e043cb42eec03465330485b33750d7a4cd3d
Neil Daftary <nmd278@nyu.edu>
2017-03-28 22:07:45 -0400

    Tidy.

--- src/App.js
+++ src/App.js
@@ -8,54 +8,43 @@ import Alert from './components/Alert';
 import Landing from './components/Landing';
 import HungryCat from './components/HungryCat';
 
-export const options = {
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Cache-Control': 'private,max-age=0,no-cache,no-store',
-        'Pragma': 'no-cache',
-        'Expires': '0'
-    }
-};
-
 class App extends Component {
-
     state = {
         loading: false,
-        done: false,
+        locationFound: false,
         position: {},
     };
 
     getFood = () => {
-        this.setState({loading: true});
+        this.setState({ loading: true });
         getLocation().then((position) => {
             this.setState({position: {
                 lat: position.coords.latitude,
                 long: position.coords.longitude,
             }}, () => {
-                this.setState({loading: false, done: true});
+                this.setState({ loading: false, locationFound: true });
             });
         });
     };
 
     render() {
-        const { loading, done, position } = this.state;
+        const { loading, locationFound, position } = this.state;
 
         return (
             <div className="App">
                 <Alert />
-                {!done && <div id="carousel" className="page-bgs">
+                {!locationFound && <div id="carousel" className="page-bgs">
                     <div style={{
                         backgroundImage: `url(${BgImage})`
                     }}></div>
                 </div>}
                 <main className="page-wrap container">
-                    {!done && <Landing
+                    {!locationFound && <Landing
                                     loading={loading}
                                     getFood={this.getFood}
                               />
                     }
-                    {done && <HungryCat position={position} />}
+                    {locationFound && <HungryCat position={position} />}
                 </main>
             </div>
         );
