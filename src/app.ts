bd4e7ae127da2823128a63d12ed2bead15045af4
Neil Daftary <nmd278@nyu.edu>
2023-02-18 14:00:04 -0500

    Add server main.

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+const port = 9000;
+
+app.get('/', (req, res) => {
+});
+
+app.get('/status', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
